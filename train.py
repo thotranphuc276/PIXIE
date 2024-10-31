129a5a5e39fcf2812d74234e3c1e1d6274caca61
thotranphuc276 <thochit2762003@gmail.com>
2024-10-31 13:07:55 +0700

    Train

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import argparse
 import torch
 import torch.optim as optim
 from tqdm import tqdm
@@ -5,6 +6,13 @@ from pixielib.pixie import PIXIE
 from pixielib.utils.config import cfg as pixie_cfg
 from loader import COCOWholeBodyDataset
 from keypoint_loss import KeypointLoss
+import random
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description="Train PIXIE model on COCOWholeBody dataset.")
+parser.add_argument('--num_samples', type=int, default=1000, help="Number of random samples to train on")
+parser.add_argument('--batch_size', type=int, default=8, help="Batch size for training")
+args = parser.parse_args()
 
 # Initialize device and model
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -22,9 +30,15 @@ for module in [pixie.Encoder, pixie.Regressor, pixie.Extractor, pixie.Moderator]
 # Optimizer setup
 optimizer = optim.Adam(params, lr=1e-4)
 
-# DataLoader setup
+# DataLoader setup with random sampling and batch size
 dataset = COCOWholeBodyDataset('coco_wholebody_train_v1.0.json')
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=True)
+if args.num_samples:
+    # Generate random indices
+    random_indices = random.sample(range(len(dataset)), args.num_samples)
+    # Create a Subset with these random indices
+    dataset = torch.utils.data.Subset(dataset, random_indices)
+
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
 
 # Training loop
 for epoch in range(10):
